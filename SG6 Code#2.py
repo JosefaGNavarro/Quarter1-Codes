041e6bd2dca3c07c02883b41ebff1cdc2e469ca1
Josefa <josefacrieza.navarro@smc.pshs.edu.ph>
2025-09-08 19:55:13 +0800

    SG6 Code#2
    This code calculates the area of a circle based on the radius given by the user.

---
+++ SG6 Code#2.py
@@ -0,0 +1,9 @@
+radius = int(input("Enter the radius of the circle:"))
+
+pi = 3.14
+
+area = pi * radius * radius
+
+area_rounded = round(area, 2)
+
+print("The area of the circle is:", area_rounded)
