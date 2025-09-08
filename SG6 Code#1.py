79e85b3fa36554cfb349e75b6bd5455dd8c896a8
Josefa <josefacrieza.navarro@smc.pshs.edu.ph>
2025-09-08 19:33:00 +0800

    SG6 Code#1
    This code will find the sum of three numbers given by the user.

---
+++ SG6 Code#1.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter the first number:"))
+
+num2 = float(input("Enter the second number:"))
+
+num3 = float(input("Enter the third number:"))
+
+sum = num1 + num2 + num3
+
+round_sum = round(sum, 2)
+
+print("The total of the three numbers is:", round_sum)
